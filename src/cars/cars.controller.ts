40db5aef22549f6f8e137ed477d6f0fe36097274
¡Duque <atanaelleonardo@gmail.com>
2024-05-16 20:24:26 -0300

    feat: "Added Get endpoint to retrieve car by ID to CarsController"

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
+import { log } from 'console'
 
 @Controller('cars')
 export class CarsController {
   private cars = ['Toyota', 'Honda', 'Jeep']
+
   @Get()
   async getAllCars() {
     return this.cars
+  }
+  @Get(':id')
+  async getCarById(@Param('id') id: string) {
+    log(id)
+    return this.cars[id]
   }
 }
